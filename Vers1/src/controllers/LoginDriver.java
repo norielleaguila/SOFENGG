dff060072d27e0396edf88deae0c67cb4c9dcbcf
Norielle Aguila <norielle_aguila@dlsu.edu.ph>
2017-10-11 00:19:59 +0800

    Finished basic LoginScreen

--- Vers1/src/controllers/LoginDriver.java
+++ Vers1/src/controllers/LoginDriver.java
@@ -2,6 +2,7 @@ package controllers;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import views.LoginScreen;
 
 public class LoginDriver extends Application{
 	
@@ -11,7 +12,9 @@ public class LoginDriver extends Application{
 	
 	@Override
 	public void start(Stage window) throws Exception {
-		
+		LoginScreen loginScreen = new LoginScreen();
+		window.setScene(loginScreen.getScene());
+		window.show();
 	}
 	
 }
--- Vers1/src/views/LoginScreen.java
+++ Vers1/src/views/LoginScreen.java
@@ -4,24 +4,25 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
 public class LoginScreen {
 	private Scene scene;
 	private VBox layout;
 	
+	private Label[] textPrompts;
 	private TextField username;
 	private PasswordField password;
 	private Button loginBtn;
 	
 	public LoginScreen(){
-		
 		setUpLayout();
 		
 		createElements();
+		
+		addToLayout();
 		
 		scene = new Scene(layout);
 	}
@@ -31,23 +32,77 @@ public class LoginScreen {
 	 */
 	public void setUpLayout(){
 		layout = new VBox(10);
-		layout.setPadding(new Insets(20, 20, 20, 20));
-		layout.setAlignment(Pos.CENTER);
+		layout.setPadding(new Insets(25, 25, 25, 25));
+		layout.setAlignment(Pos.CENTER_LEFT);
 	}
 	
 	/*
 	 * createElements instantiates the elements of this view 
 	 */
 	public void createElements(){
+		// initialize text prompts
+		textPrompts = new Label[]{new Label("Username:"), new Label("Password:")};
+		
+		// create username field
 		username = new TextField();
+		username.setPrefSize(200, 20);
+		
+		// create password field
 		password = new PasswordField();
+		password.setPrefSize(200, 20);
+		
+		// create login button
 		loginBtn = new Button("Login");
+		loginBtn.setPrefSize(200, 20);
 	}
 	
 	public void addToLayout(){
+		layout.getChildren().add(textPrompts[0]);
 		layout.getChildren().add(username);
+		layout.getChildren().add(textPrompts[1]);
 		layout.getChildren().add(password);
 		layout.getChildren().add(loginBtn);
 	}
+
+	public Scene getScene() {
+		return scene;
+	}
+
+	public void setScene(Scene scene) {
+		this.scene = scene;
+	}
+
+	public VBox getLayout() {
+		return layout;
+	}
+
+	public void setLayout(VBox layout) {
+		this.layout = layout;
+	}
+
+	public TextField getUsername() {
+		return username;
+	}
+
+	public void setUsername(TextField username) {
+		this.username = username;
+	}
+
+	public PasswordField getPassword() {
+		return password;
+	}
+
+	public void setPassword(PasswordField password) {
+		this.password = password;
+	}
+
+	public Button getLoginBtn() {
+		return loginBtn;
+	}
+
+	public void setLoginBtn(Button loginBtn) {
+		this.loginBtn = loginBtn;
+	}
+	
 	
 }
