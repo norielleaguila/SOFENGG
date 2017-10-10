37c755ba821d54fa65f9e4dc8c0c57186fec7750
Norielle Aguila <norielle_aguila@dlsu.edu.ph>
2017-10-10 23:48:58 +0800

    Added elements to LoginScreen

---
+++ Vers1/src/controllers/LoginDriver.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LoginDriver extends Application{
+	
+	public static void main(String[] args){
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage window) throws Exception {
+		
+	}
+	
+}
