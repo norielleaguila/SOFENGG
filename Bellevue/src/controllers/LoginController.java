e466b8510d77a0cc977cd3b4e3a9de425ae984b6
Norielle Aguila <norielle_aguila@dlsu.edu.ph>
2017-10-18 22:51:05 +0800

    Added enter login functionality

--- Bellevue/src/controllers/LoginController.java
+++ Bellevue/src/controllers/LoginController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import javafx.scene.input.KeyCode;
 import views.LoginScreen;
 
 /**
@@ -9,33 +10,45 @@ import views.LoginScreen;
  */
 public class LoginController extends Controller{
 	private LoginScreen view;
+	private String user = "admin", pass = "123";
 	
 	public LoginController(LoginScreen view){
 		this.view = view;
 		setUpButtons();
+		onEnterLogin();
+	}
+	
+	public void onEnterLogin(){
+		view.getScene().setOnKeyPressed(e -> {
+			if(e.getCode().equals(KeyCode.ENTER)){
+				validate();
+			}
+		});
 	}
 	
 	public void setUpButtons(){
-		String user = "admin", pass = "123";
-		
 		view.getLoginBtn().setOnAction(e -> {
-			/* empty failure condition */
-			if(view.getUsername().getText().equals("") || view.getUsername().getText() == null){
-				view.setNotif("Username cannot be empty!");
-			}
-			else if(view.getPassword().getText().equals("") || view.getPassword().getText() == null){
-				view.setNotif("Password cannot be empty!");
-			}
-			
-			/* invalid credentials condition */
-			else if(!view.getUsername().getText().equals(user) || !view.getPassword().getText().equals(pass)){
-				view.setNotif("Invalid login.");
-			}
-			
-			else{
-				view.setNotif("Welcome, " + view.getUsername().getText() + "!");
-			}
-			view.resetLayout();
+			validate();
 		});
+	}
+	
+	public void validate(){
+		/* empty failure condition */
+		if(view.getUsername().getText().equals("") || view.getUsername().getText() == null){
+			view.setNotif("Username cannot be empty!");
+		}
+		else if(view.getPassword().getText().equals("") || view.getPassword().getText() == null){
+			view.setNotif("Password cannot be empty!");
+		}
+		
+		/* invalid credentials condition */
+		else if(!view.getUsername().getText().equals(user) || !view.getPassword().getText().equals(pass)){
+			view.setNotif("Invalid login.");
+		}
+		
+		else{
+			view.setNotif("Welcome, " + view.getUsername().getText() + "!");
+		}
+		view.resetLayout();
 	}
 }
