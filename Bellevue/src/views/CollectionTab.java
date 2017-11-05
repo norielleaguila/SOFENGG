df8226c83fecd16700b5d2355ce6ed6751c51ab7
Norielle Aguila <norielle_aguila@dlsu.edu.ph>
2017-11-05 21:21:23 +0800

    started collection table

--- Bellevue/src/views/CollectionTab.java
+++ Bellevue/src/views/CollectionTab.java
@@ -1,12 +1,27 @@
 package views;
 
+import javafx.scene.layout.BorderPane;
+
 /**
  * @author AGUILA, Norielle
  */
 
 public class CollectionTab extends Tabs{
 	
+	private BorderPane layout;
+	
 	public CollectionTab(){
 		super();
+		
+		layout = new BorderPane();
+		
+		
+		
 	}
+	
+	public void initLayout(){
+//		layout.setCenter(new CollectionTable());
+		
+	}
+	
 }
