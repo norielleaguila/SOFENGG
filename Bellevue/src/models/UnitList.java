922c2f526b36e9c3dc1d9589d1bcdb62f2048f9c
Norielle Aguila <norielle_aguila@dlsu.edu.ph>
2017-11-27 01:46:56 +0800

    added filtering logic

--- Bellevue/src/models/UnitList.java
+++ Bellevue/src/models/UnitList.java
@@ -77,4 +77,62 @@ public class UnitList extends Model{
 		return searchResults;
 	}
 	
+	/**
+	 * Filters all units into either paid, unpaid, or both through a filter parameter. 
+	 * Will only be called when the {@code All} radio button is unselected.
+	 * @param filterParameter 
+	 * 			<p>	The {@code String} parameter(s) which the unit list will be filtered by.
+	 * 				Parameters are split by space as its delimiter. </p>
+	 * 			<p>	Example filter parameters: {@code "paid"}, {@code "paid unpaid"}. </p>
+	 * @return <b>filterResults</b>
+	 * 			<p>	{@code ArrayList} of {@code Units} that match the given filter parameter.</p>
+	 * 
+	 */
+	public ArrayList<Unit> filterUnits(String filterParameter){
+		ArrayList<Unit> filterResults = new ArrayList<Unit>();
+		
+		// if units are to be filtered by more than 1, the String will be split with a space as its delimiter
+		String[] filterByList =  filterParameter.split(" ");
+		
+		for(String filterBy : filterByList){
+			switch(filterBy){
+			case "paid":
+				filterResults.addAll(getAllPaidUnits());
+				break;
+			case "unpaid":
+				filterResults.addAll(getAllUnpaidUnits());
+				break;
+			case "overdue":
+				// unsure if logical to still have this
+				break;
+			default:
+				// error, filter parameter incorrect
+			}
+		}
+		
+		return filterResults;
+	}
+	
+	public ArrayList<Unit> getAllPaidUnits(){
+		ArrayList<Unit> paidUnits = new ArrayList<Unit>();
+		
+		for(Unit unit : units){
+			if(unit.isPaid())
+				paidUnits.add(unit);
+		}
+		
+		return paidUnits;
+	}
+	
+	public ArrayList<Unit> getAllUnpaidUnits(){
+		ArrayList<Unit> paidUnits = new ArrayList<Unit>();
+		
+		for(Unit unit : units){
+			if(!unit.isPaid())
+				paidUnits.add(unit);
+		}
+		
+		return paidUnits;
+	}
+	
 }
